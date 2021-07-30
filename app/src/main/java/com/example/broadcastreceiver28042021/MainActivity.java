9bb8f1856146091de4abbf55a14c55ebb6f55291
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-30 21:19:24 +0700

    check internet

--- app/src/main/java/com/example/broadcastreceiver28042021/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver28042021/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.broadcastreceiver28042021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyBroadCastReceiver myBroadCastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myBroadCastReceiver = new MyBroadCastReceiver();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        intentFilter.addAction("android.net.wifi.WIFI_STATE_CHANGED");
+        registerReceiver(myBroadCastReceiver ,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(myBroadCastReceiver);
     }
 }
