6e5a60e40cc0e3d503464f1feb546c549450ddb1
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-07-30 20:58:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/broadcastreceiver28042021/MyBroadCastReceiver.java
@@ -0,0 +1,21 @@
+package com.example.broadcastreceiver28042021;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class MyBroadCastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getActiveNetworkInfo().getState() == NetworkInfo.State.CONNECTED){
+            //we are connected to a network
+            Toast.makeText(context, "Có internet", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Mất internet", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
